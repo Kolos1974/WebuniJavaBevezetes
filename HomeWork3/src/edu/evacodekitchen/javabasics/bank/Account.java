3f3b8c4ed95ffa1814ebb48b9c6d3b11ab4e45be
Kolos <karlovitz.kolos@gmail.com>
2024-04-26 12:53:07 +0200

    HomeWork3

--- HomeWork3/src/edu/evacodekitchen/javabasics/bank/Account.java
+++ HomeWork3/src/edu/evacodekitchen/javabasics/bank/Account.java
@@ -3,13 +3,17 @@ package edu.evacodekitchen.javabasics.bank;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.function.Predicate;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class Account {
 
-	String owner;
-	String id;
-	int balance = 0;
-	List<Transaction> transactions = new ArrayList<Transaction>();
+	// private int balance = 0;
+	private String owner;
+	private String id;
+	private List<Transaction> transactions = new ArrayList<Transaction>();
 
 	public Account(String owner, String id) {
 		this.owner = owner;
@@ -17,28 +21,32 @@ public class Account {
 	}
 
 	public int getBalance() {
+		/*
 		int sum = 0;
 		for (Transaction transaction : transactions) {
 			sum += transaction.getAmount();
 		}
 		return sum;
+		*/
+		
+		return transactions.stream().mapToInt(t -> t.getAmount()).sum(); 
+		
+		
 	}
 
 	public void doTransaction(int amount, LocalDate date) {
-		if (amount >= 0) {
-			transactions.add(new Transaction(amount, date));
+		
+		if ((getBalance() + amount) < 0) {
+			throw new TooLowBalanceException();
 		}
 		else {
-			if ((getBalance() + amount) >= 0) {
-				transactions.add(new Transaction(amount, date));
-			}
-			else {
-				throw new TooLowBalanceException();
-			}
+			transactions.add(new Transaction(amount, date));
 		}
+		
 	}
 
 	public List<Transaction> getTransactionsFor(LocalDate date) {
+		/*
 		List<Transaction> result = new ArrayList<>();
 		for (Transaction transaction : transactions) {
 			if (transaction.getDate().equals(date)){
@@ -46,6 +54,30 @@ public class Account {
 			}
 		}
 		return result;
+		*/
+		
+		// return transactions.stream().filter((Predicate<Transaction>) t -> t.getDate().equals(date)).collect(Collectors.toList());
+		
+		return transactions.stream().filter(t -> t.getDate().equals(date)).collect(Collectors.toList()); 
 	}
 
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, owner);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Account other = (Account) obj;
+		return Objects.equals(id, other.id) && Objects.equals(owner, other.owner);
+	}
+
+	
+	
 }
--- HomeWork3/src/edu/evacodekitchen/javabasics/bank/Bank.java
+++ HomeWork3/src/edu/evacodekitchen/javabasics/bank/Bank.java
@@ -1,21 +1,51 @@
 package edu.evacodekitchen.javabasics.bank;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class Bank {
 
+	private List<Account> accounts = new ArrayList<Account>();
+	
 	public void addAccount(Account account) {
 		// TODO
+		accounts.add(account);
 	}
 
 	public List<Account> getAccounts() {
 		// TODO
-		return null;
+		/*
+		List<Account> result = new ArrayList<Account>();
+		for (Account account : accounts) {
+			result.add(account);
+		}
+		return result;
+		*/
+		
+		// return accounts.stream().collect(Collectors.toList());
+		
+		return accounts;
+		
 	}
 
 	public List<Account> getPremiumAccounts() {
 		// TODO
-		return null;
+		/*
+		List<Account> result = new ArrayList<Account>();
+		for (Account account : accounts) {
+			if (account.getBalance()>12000) {
+				result.add(account);
+			}
+		}
+		return result;
+		*/
+		
+		// return accounts.stream().filter((Predicate<Account>) a -> a.getBalance()>12000).collect(Collectors.toList());
+		
+		return accounts.stream().filter(a -> a.getBalance()>12000).collect(Collectors.toList());
+		
 	}
 
 }
